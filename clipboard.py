0f616eeb1931ee7885b693be4df25c439e0ff910
Spencer Pearson <suspense@cs.washington.edu>
2017-10-06 14:45:37 -0700

    make utf-8 encoding explicit

--- clipboard.py
+++ clipboard.py
@@ -6,12 +6,12 @@ class UnknownPlatformError(Exception):
 
 def copy_to_clipboard(s):
   if sys.platform.startswith('linux'):
-    subprocess.Popen(['xsel', '--input', '--clipboard'], stdin=subprocess.PIPE).communicate(s.encode())
+    subprocess.Popen(['xsel', '--input', '--clipboard'], stdin=subprocess.PIPE).communicate(s.encode('utf-8'))
   elif sys.platform.startswith('darwin'):
-    subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE).communicate(s.encode())
+    subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE).communicate(s.encode('utf-8'))
   elif sys.platform.startswith('win32'):
-    subprocess.Popen(['clip'], stdin=subprocess.PIPE).communicate(s.encode())
+    subprocess.Popen(['clip'], stdin=subprocess.PIPE).communicate(s.encode('utf-8'))
   elif sys.platform.startswith('cygwin'):
-    open('/dev/clipboard', 'w').write(s.encode())
+    open('/dev/clipboard', 'w').write(s.encode('utf-8'))
   else:
     raise UnknownPlatformError(sys.platform)
