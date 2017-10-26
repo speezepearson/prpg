6616ea41a9f4d732a176971af8e6f8c9d9baf4b5
Spencer Pearson <suspense@cs.washington.edu>
2017-10-25 23:50:17 -0700

    have cli.salts.get print chosen salt

--- prpg/cli/salts/get.py
+++ prpg/cli/salts/get.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+import sys
 import json
 import pprint
 from ..common_args import add_query_argument
@@ -6,6 +8,7 @@ from ...saltfiles import load_salts, disambiguate, fuzzy_search
 def main(args):
   salts = load_salts(args.salt_file)
   salt = disambiguate(fuzzy_search(salts, args.query))
+  print('Chosen salt: {!r}'.format(salt), file=sys.stderr)
   pprint.pprint(salts[salt])
 
 def prepare_parser(parser):
