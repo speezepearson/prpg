ad2bfa03592722cfde40d41e1aaf747ecd13943c
Spencer Pearson <suspense@cs.washington.edu>
2017-11-21 11:28:21 -0800

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.2',
+    version='0.4',
 
     description='A pseudorandom password generator / password manager.',
     long_description=long_description,
@@ -48,6 +48,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
